91441bf1a6aaf2d3b864e7efef84bfcb9bdfed5c
mingyeahh <mingyeahh@users.noreply.github.com>
2022-11-02 00:55:22 +0000

    make gpt-3 work with an interface now!

--- client/index.js
+++ client/index.js
@@ -24,6 +24,7 @@ frm.addEventListener("submit", (e) => {
         },
     }).then((res) => {
         if (res.ok) {
+            frm.reset();
             return res.json();
         } else {
             console.error(res.body);
@@ -31,6 +32,10 @@ frm.addEventListener("submit", (e) => {
         }
     }).then(data => {
         console.log(data);
+        let newLine = `<span class="title">Human:</span> ${hinp}
+                    <br><span class="title">AI:</span> ${data.text}
+                    <br>`;
+        document.getElementById('conv-model1').innerHTML += newLine;
     }).catch(console.error);
 });
 
@@ -49,5 +54,11 @@ window.addEventListener("load", (e) => {
         }
     }).then(data => {
         console.log(data);
+        document.getElementById('conv-model1').innerHTML = '';
+        data.forEach((i) =>{
+            let newLine = `<span class="title">${i.sender}:</span> ${i.msg}
+                        <br>`;
+            document.getElementById('conv-model1').innerHTML += newLine;
+        });
     }).catch(console.error);
 });
