82a7e9699d92d1cbd08ee00be7ef82c6d12906cd
mingyeahh <mingyeahh@users.noreply.github.com>
2022-11-11 00:14:56 +0000

    Reconstruct server

--- app.js
+++ app.js
@@ -1,41 +1,59 @@
 const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
 const fs = require("fs");
-let history = require("./history.json");
-
-const historyToPrompt = (hist) => {
-    let buffer = "";
-    // hist.forEach((h) => {
-    //     buffer = buffer.concat(`${h.sender}: ${h.msg}\n`);
-    // });
-    return buffer;
-}
-
-const port = 8080;
-
 const app = express();
 app.use(express.json()); // parse JSON requests
 app.use(express.static("client"));
 
-// console.log(history); // DEBUG
+// let history = require("./history.json"); // list of past conversation
 
+class History{
+    constructor(index, buffer, batchSize, histPath){
+        this.index = index;
+        this.buffer = buffer;
+        this.batchSize = batchSize;
+        this.histPath = histPath;
+        this.listory = require(this.histPath);
+        this.currentSummary = "";
+    }
+
+    historyToText(start=0, end=-1) {
+        if (end < 0) {end = this.listory.length}
+        let buffer = "";
+        for (let i = start; i<end; i++){
+            let h = this.listory[i];
+            buffer = buffer.concat(`${h.sender}: ${h.msg}\n`);
+        }
+        return buffer;
+    }
+
+    push(sender, msg, time) {
+        this.listory.push({
+            sender: sender,
+            msg: msg,
+            time: time,
+        });
+        fs.writeFileSync(this.histPath, JSON.stringify(this.listory));
+        if (this.listory.length >= this.index + this.batchSize + this.buffer){
+            // do summary thing
+        }
+    }
+    conversationPrompt(){
+        let builtInText = "We'll be learning about NLP, we've already discussed:";
+        let restHist = this.historyToText(this.index);
+        return `${builtInText} ${this.currentSummary}\n\n${restHist}AI:`;
+    }
+}
+let conversation = new History(0, 20, 20, "./history.json");
+
+
+// Apply GTP-3 to answer questions based on input conversation
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-
-// List of topics that have been discussed 
-let list_discussed = ['Transformer', 'LSTM', 'Large language models']
-
-const discussed_item = (list_discussed) =>{
-    let temp = [...list_discussed];
-    let last = temp.pop(); 
-    let str = temp.join(', ');
-    return `${str}, and ${last}`;
-}
-
-
+// Response from GPT-3 based on prompt and user input
 app.post("/send", (req, res) => {
     let msg = req.body.msg;
     if (msg === undefined || msg.length === 0) {
@@ -43,32 +61,18 @@ app.post("/send", (req, res) => {
         res.end();
         return;
     }
-    history.push({
-        sender: "human",
-        msg: msg,
-        time: Math.floor(Date.now() / 1000),
-    });
+    conversation.push("human", msg, Math.floor(Date.now() / 1000));
     openai.createCompletion({
         model: "text-davinci-002",
-        prompt:
-`We'll be learning about NLP, we've already discussed: ${(discussed_item(list_discussed))}. Now we will discuss more things related to NLP. You should be leading the conversation by asking humans what they know first, and teaching the human something they never know.
-${historyToPrompt(history)}
-Human: ${msg}
-AI:`,
+        prompt:conversation.conversationPrompt(),
         temperature: 0.9,
         max_tokens: 150,
         stop: ["Human:", "AI:"],
     }).then(gpt => {
-        console.log(gpt.data.choices[0].text);
-        history.push({
-            sender: "AI",
-            msg: gpt.data.choices[0].text,
-            time: gpt.data.created,
-        });
+        conversation.push("AI", gpt.data.choices[0].text, gpt.data.created);
         res.send({
             text: gpt.data.choices[0].text,
         });
-        fs.writeFileSync("history.json", JSON.stringify(history));
     }).catch(err => {
         console.log(err);
         res.status(500);
@@ -80,10 +84,46 @@ app.post('*', (req, res) => {
     console.log(req);
 });
 
+// Send past conversation to log in front-end from server side
 app.get('/history', (req, res) =>{
-    res.send(history);
+    res.send(conversation.listory);
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+// List of summerisation 
+let list_discussed = ['Transformer']
+
+// const configuration2 = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai2 = new OpenAIApi(configuration2);
+
+//     openai2.createCompletion({
+//         model: "text-davinci-002",
+//         prompt:
+// `Summerise ${(discussed_item(list_discussed))} in details.`,
+//         temperature: 0.7,
+//         max_tokens: 256,
+//         top_p: 1,
+//         frequency_penalty: 0,
+//         presence_penalty: 0,
+//     })
+
+// const response = await openai2.createCompletion({
+//   model: "text-davinci-002",
+//   prompt: "Summarize ",
+//   temperature: 0.7,
+//   max_tokens: 256,
+//   top_p: 1,
+//   frequency_penalty: 0,
+//   presence_penalty: 0,
+// });
+
+
+const discussed_item = (list_discussed) =>{
+    let temp = [...list_discussed];
+    let last = temp.pop(); 
+    let str = temp.join(', ');
+    return `${str}, and ${last}`;
+}
+
+module.exports = app;
